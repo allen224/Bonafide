a13d945887994f1f283d628bd7e676802696e874
rhenz <salvador.rhenz@gmail.com>
2014-07-02 16:10:37 +0800

    Always change the “Created by” and “Copyright” in the comment area to Bonafide Infotech Inc.

--- Bonafide/BITViewController.h
+++ Bonafide/BITViewController.h
@@ -2,8 +2,8 @@
 //  BITViewController.h
 //  Bonafide
 //
-//  Created by Rhenz on 7/2/14.
-//  Copyright (c) 2014 John Salvador. All rights reserved.
+//  Created by Bonafide Infotech Inc. on 7/2/14.
+//  Copyright (c) 2014 Bonafide Infotech Inc. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
